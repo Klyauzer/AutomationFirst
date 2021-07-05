103dcfc97a426fb0622f4d0405be5fe0646f7f29
Klyauzer <storm9945@gmail.com>
2021-07-05 17:52:02 +0300

    Lesson 4 Cucumber Done

--- FirstProject/src/test/java/page/TaskSix.java
+++ FirstProject/src/test/java/page/TaskSix.java
@@ -2,8 +2,12 @@ package page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 import static java.lang.Thread.sleep;
+import static org.junit.Assert.assertFalse;
 
 public class TaskSix extends BasePage {
     public TaskSix(WebDriver driver) {
@@ -16,18 +20,23 @@ public class TaskSix extends BasePage {
         return this;
     }
 
-    public TaskSix fillInPassword(String password){
+    public TaskSix fillInPassword(String password) {
         writeText(By.name("LoginForm[_password]"), password);
         return this;
     }
 
-    public TaskSix loginButtonClick(){
+    public TaskSix loginButtonClick() {
         click(By.xpath("//button[text()='Login']"));
         return this;
     }
 
-    public TaskSix isLoginCorrect(){
-        driver.findElement(By.linkText("Pobierz plik")).isDisplayed();
+    public TaskSix isLoginCorrect() {
+        isElementDisplayed(By.linkText("Pobierz plik"));
+        return this;
+    }
+
+    public TaskSix isLoginWrong() {
+        isElementNotDisplayed(By.linkText("Pobierz plik"));
         return this;
     }
 }
--- FirstProject/src/test/java/test/TestBase.java
+++ FirstProject/src/test/java/test/TestBase.java
@@ -1,8 +1,7 @@
 package test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -20,18 +19,17 @@ public class TestBase {
     public TaskSix taskSix;
 
 
-    @BeforeEach
     public void start(){
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         main = PageFactory.initElements(driver, Main.class);
         taskOne = PageFactory.initElements(driver, TaskOne.class);
         taskSix = PageFactory.initElements(driver, TaskSix.class);
     }
 
-    @AfterEach
     public void finish(){
         driver.quit();
     }
