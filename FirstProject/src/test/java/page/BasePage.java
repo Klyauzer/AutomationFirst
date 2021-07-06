9b913353420f96d5c8b3f97ba87a7e3b80ba376e
Klyauzer <storm9945@gmail.com>
2021-07-06 14:01:49 +0300

    NewRepository

--- FirstProject/src/test/java/page/BasePage.java
+++ FirstProject/src/test/java/page/BasePage.java
@@ -1,12 +1,13 @@
 package page;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
@@ -16,35 +17,44 @@ public class BasePage {
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
-        wait = new WebDriverWait(driver,15);
+        wait = new WebDriverWait(driver, 15);
     }
 
-    //Wait Wrapper Method
-    public void waitVisability(By elementBy) {
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(elementBy));
+    //Wait Wrapper Method  by element located By
+    public void waitVisibility(By elementBy) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(elementBy));
     }
 
-    //Click Method
-    public void click (By elementBy) {
-        waitVisability(elementBy);
+    //Click Method by element located By
+    public void click(By elementBy) {
+        waitVisibility(elementBy);
         driver.findElement(elementBy).click();
     }
 
-    //Is Element Displayed
-    public void isElementDisplayed (By elementBy) {
-        waitVisability(elementBy);
+    //Is Element located By  Displayed
+    public void isElementDisplayed(By elementBy) {
+        waitVisibility(elementBy);
         assertTrue(driver.findElement(elementBy).isDisplayed());
     }
 
+    public void waitForFilling(By elementBy) {
+        wait.until(ExpectedConditions.elementToBeClickable(elementBy));
+    }
+
     //Write Text in field located By
-    public void writeText(By elementBy, String text){
-        waitVisability(elementBy);
+    public void writeText(By elementBy, String text) {
+        waitVisibility(elementBy);
         WebElement element = driver.findElement(elementBy);
         element.clear();
         element.sendKeys(text);
     }
 
-    public void isElementNotDisplayed (By elementBy) {
-        assertFalse(driver.findElements(elementBy).size() > 0);
+    public void isElementNotDisplayed(By elementBy) {
+        assertTrue(driver.findElements(elementBy).isEmpty());
     }
+
+    public void isAlertText(String message) {
+        assertEquals(message, driver.switchTo().alert().getText());
+    }
+
 }
