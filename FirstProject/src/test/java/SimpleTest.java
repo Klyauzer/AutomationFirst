cdfa7c1a3874f5e083cdf8241202cd68390bc84c
Klyauzer <storm9945@gmail.com>
2021-07-08 12:04:44 +0300

    Lesson 7 Finish

--- FirstProject/src/test/java/SimpleTest.java
+++ FirstProject/src/test/java/SimpleTest.java
@@ -20,7 +20,7 @@ public class SimpleTest {
     WebDriver driver;
 
     @BeforeEach
-    public void start(){
+    public void start() {
         WebDriverManager.firefoxdriver().setup();
         FirefoxOptions options = new FirefoxOptions();
         options.setHeadless(true);
@@ -42,20 +42,56 @@ public class SimpleTest {
     @Story(value = "Test for login with correct credentials")
 
     @Test
-    public void testSixCheckLoginWithCorrectCredentials(){
-        driver.findElement(By.linkText("Zadanie 6")).click();
-        driver.findElement(By.id("LoginForm__username")).sendKeys("tester");
-        driver.findElement(By.id("LoginForm__password")).sendKeys("123-xyz");
-        driver.findElement(By.id("LoginForm_save")).click();
-        assertTrue(driver.findElements(By.linkText("Pobierz plik")).isEmpty());
+    public void testSixCheckLoginWithCorrectCredentials() {
+        chooseTaskSix();
+        fillInLogin("tester");
+        fillInPassword("123-xyz");
+        loginButtonClick();
+        isLoginSuccessful();
     }
 
+    @Epic("TESTING FOR https://testingcup.pgs-soft.com/ tasks")
+    @Feature(value = "Tests for task 6")
+    @Severity(SeverityLevel.MINOR)
+    @Description("In this test we will login with correct credentials. When we logged we see link for file.")
+    @Story(value = "Test for login with incorrect credentials")
+
     @Test
-    public void testSixCheckLoginWithIncorrectCredentials(){
-        driver.findElement(By.linkText("Zadanie 6")).click();
-        driver.findElement(By.id("LoginForm__username")).sendKeys("tester");
-        driver.findElement(By.id("LoginForm__password")).sendKeys("12,4-xyw");
+    public void testSixCheckLoginWithIncorrectCredentials() {
+        chooseTaskSix();
+        fillInLogin("tester");
+        fillInPassword("123-xyzxx");
+        loginButtonClick();
+        isLoginUnsuccessful();
+    }
+
+    @Step(value = "Fill in login with {0}")
+    public void fillInLogin(String login) {
+        driver.findElement(By.id("LoginForm__username")).sendKeys(login);
+    }
+
+    @Step(value = "Fill in password with {0}")
+    public void fillInPassword(String password) {
+        driver.findElement(By.id("LoginForm__password")).sendKeys(password);
+    }
+
+    @Step(value = "Click button Login")
+    public void loginButtonClick() {
         driver.findElement(By.id("LoginForm_save")).click();
+    }
+
+    @Step(value = "Choose task 6 on main page")
+    public void chooseTaskSix() {
+        driver.findElement(By.linkText("Zadanie 6")).click();
+    }
+
+    @Step(value = "Login was successful")
+    public void isLoginSuccessful() {
+        assertTrue(driver.findElement(By.linkText("Pobierz plik")).isDisplayed());
+    }
+
+    @Step(value = "Login was unsuccessful")
+    public void isLoginUnsuccessful() {
         assertTrue(driver.findElements(By.linkText("Pobierz plik")).isEmpty());
     }
 
