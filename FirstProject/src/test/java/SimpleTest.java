d4a1f190168d99c151ffbd081e01761186b185d9
Klyauzer <storm9945@gmail.com>
2021-07-08 16:35:21 +0300

    Lesson 8 Finish

--- FirstProject/src/test/java/SimpleTest.java
+++ FirstProject/src/test/java/SimpleTest.java
@@ -1,98 +1,79 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import io.qameta.allure.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.*;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static java.lang.Thread.sleep;
+import static org.testng.AssertJUnit.assertTrue;
 
 public class SimpleTest {
-    final String SITE_URL = "https://testingcup.pgs-soft.com/";
     WebDriver driver;
+    final String SITE_URL = "https://testingcup.pgs-soft.com/";
+    final String USER_PROFILE_LINK = "https://testingcup.pgs-soft.com/task_6/logged";
+    Set<Cookie> cookies;
 
-    @BeforeEach
-    public void start() {
-        WebDriverManager.firefoxdriver().setup();
-        FirefoxOptions options = new FirefoxOptions();
-        options.setHeadless(true);
-        driver = new FirefoxDriver(options);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
+    @BeforeClass
+    @Parameters("browser")
+    public void initialization(String browser) {
+        if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            FirefoxOptions options = new FirefoxOptions();
+            options.setHeadless(true);
+            driver = new FirefoxDriver(options);
+        } else if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.setHeadless(true);
+            driver = new ChromeDriver(options);
+        }
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get(SITE_URL);
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                driver.manage().addCookie(cookie);
+            }
+        }
     }
 
-    @AfterEach
-    public void finish() {
+    @AfterClass
+    public void tearDown() {
         driver.quit();
     }
 
-    @Epic("TESTING FOR https://testingcup.pgs-soft.com/ tasks")
-    @Feature(value = "Tests for task 6")
-    @Severity(SeverityLevel.BLOCKER)
-    @Description("In this test we will login with correct credentials. When we logged we see link for file.")
-    @Story(value = "Test for login with correct credentials")
 
-    @Test
-    public void testSixCheckLoginWithCorrectCredentials() {
-        chooseTaskSix();
-        fillInLogin("tester");
-        fillInPassword("123-xyz");
-        loginButtonClick();
-        isLoginSuccessful();
-    }
-
-    @Epic("TESTING FOR https://testingcup.pgs-soft.com/ tasks")
-    @Feature(value = "Tests for task 6")
-    @Severity(SeverityLevel.MINOR)
-    @Description("In this test we will login with correct credentials. When we logged we see link for file.")
-    @Story(value = "Test for login with incorrect credentials")
-
-    @Test
-    public void testSixCheckLoginWithIncorrectCredentials() {
-        chooseTaskSix();
-        fillInLogin("tester");
-        fillInPassword("123-xyzxx");
-        loginButtonClick();
-        isLoginUnsuccessful();
-    }
-
-    @Step(value = "Fill in login with {0}")
-    public void fillInLogin(String login) {
-        driver.findElement(By.id("LoginForm__username")).sendKeys(login);
-    }
-
-    @Step(value = "Fill in password with {0}")
-    public void fillInPassword(String password) {
-        driver.findElement(By.id("LoginForm__password")).sendKeys(password);
-    }
-
-    @Step(value = "Click button Login")
-    public void loginButtonClick() {
-        driver.findElement(By.id("LoginForm_save")).click();
-    }
-
-    @Step(value = "Choose task 6 on main page")
-    public void chooseTaskSix() {
+    @Test(description = "Login and see logout link", priority = 1)
+    public void testOne() {
         driver.findElement(By.linkText("Zadanie 6")).click();
+        driver.findElement(By.id("LoginForm__username")).sendKeys("tester");
+        driver.findElement(By.id("LoginForm__password")).sendKeys("123-xyz");
+        driver.findElement(By.id("LoginForm_save")).click();
+        assertTrue(driver.findElement(By.id("logout")).isDisplayed());
+        cookies = driver.manage().getCookies();
     }
 
-    @Step(value = "Login was successful")
-    public void isLoginSuccessful() {
-        assertTrue(driver.findElement(By.linkText("Pobierz plik")).isDisplayed());
+    @Test(description = "Use cookies from previous test and go to profile. Check if logout link is displayed",
+            priority = 2)
+    public void loginWithoutLogin() {
+        driver.get(USER_PROFILE_LINK);
+        assertTrue(driver.findElement(By.id("logout")).isDisplayed());
+        driver.findElement(By.id("logout")).click();
+
     }
 
-    @Step(value = "Login was unsuccessful")
-    public void isLoginUnsuccessful() {
-        assertTrue(driver.findElements(By.linkText("Pobierz plik")).isEmpty());
+    @Test(description = "Use cookies from first test to go to profile and logout. Check if login link is displayed.",
+            priority = 3)
+    public void loginSecondAfterLogout() {
+        driver.get(USER_PROFILE_LINK);
+        assertTrue(driver.findElements(By.id("logout")).isEmpty());
     }
-
 }
